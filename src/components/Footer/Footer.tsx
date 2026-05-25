7c1b525a41da88e1c4ba19a091876814468e0c08
Gabriel Gonçalves Maciel <gabrielggm60@gmail.com>
2026-05-25 00:29:47 -0300

    ajustes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      © 2026 Honda Prototype - Projeto acadêmico
+      © 2026 Honda Prototype - Esse site é apenas um protótipo e não representa a Honda de forma oficial.
     </footer>
   )
 }
